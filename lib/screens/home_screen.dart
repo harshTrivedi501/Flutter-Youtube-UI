5dbf35291d857febe848b1e1d52bff4547faf8dd
Harsh Trivedi <harshtrivedi74@gmail.com>
2021-06-29 12:03:30 +0530

    Added Custom Sliver App Bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube_ui/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          CustomSliverAppBar(),
+        ],
+      ),
+    );
   }
 }
